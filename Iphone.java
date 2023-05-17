544515f1a1fbfef4e4b8582ee33393ec361f796e
AshaSinghThakur <133910844+AshaSinghThakur@users.noreply.github.com>
2023-05-17 18:34:03 -0400

    Add files via upload

---
+++ Iphone.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Iphone {
+
+	public void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("Make call method calling");
+
+	}
+	public void model() {
+		// TODO Auto-generated method stub
+		System.out.println("Model method calling");
+
+	}
+
+}
---
+++ SmartPhone.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class SmartPhone {
+	public static void main(String[] args) {
+		Iphone I=new Iphone();
+		I.makeCall();
+		I.model();
+	}
+
+}
