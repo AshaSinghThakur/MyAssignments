b83c2675aa49aa763c7820eda943aad7f438784f
AshaSinghThakur <133910844+AshaSinghThakur@users.noreply.github.com>
2023-05-30 19:56:52 -0400

    Add files via upload

---
+++ week2/Week2Day3/TestBrowser.java
@@ -0,0 +1,11 @@
+package Week2Day3;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBrowser {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+	}
+
+}
